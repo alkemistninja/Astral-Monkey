a66cf95b031d4b8b2c8d01548f250a6c49cb1a3b
Julian Reyes <Julian@Julians-Air.lan>
2018-04-18 16:34:21 -0400

    setting env variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate, only: [:admin, :new, :create, :edit, :update, :destroy]
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -45,6 +45,19 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to root_path, notice: "Post destroyed"
+  end
+
+  def admin
+    redirect_to root_path if authenticate
+  end
+
+protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      admin_username = Rails.application.secrets.admin_username
+      admin_password = Rails.application.secrets.admin_password
+      username == admin_username && password == admin_password
+    end
   end
 
 private
