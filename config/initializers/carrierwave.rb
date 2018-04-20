4ec4297c3e0ecb67929ca9ccf92f823d2fff3914
Julian Reyes <Julian@Julians-Air.home>
2018-04-20 10:58:20 -0400

    aws fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS'
-      aws_access_key_id:     ENV[""]
-      aws_secret_key_id:     ENV[""]
+      aws_access_key_id:     ENV["S3_ACCESS_KEY"]
+      aws_secret_key_id:     ENV["S3_SECRET_KEY"]
     }
     config.fog_directory = ENV["S3_BUCKET"]
   end
